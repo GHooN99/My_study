650fd58473271146321eb41b6668303d0321df10
GHooN99 <whiteblue0316@gmail.com>
2020-07-01 00:31:39 +0900

    Add BOJ_2108.py

--- 알고리즘 문제 풀이/BOJ/2108.py
+++ 알고리즘 문제 풀이/BOJ/2108.py
@@ -1,50 +1,42 @@
 from sys import stdin
 
-def find(s):
-    cnt =[]
-    result=[]
-    for i in s:
-        cnt.append(s.count(i))
-    max_cnt = max(cnt)
-    for i in range(len(cnt)):
-        if cnt[i]==max_cnt:
-            result.append(s[i])
-
-    result=list(set(result))
-    if len(result)==1:
-        return result[0]
-    else:
-        result.sort()
-        return result[1]
-        
-
-
-
-
-# 1 2 3 3 4 5 5 
 n=int(stdin.readline())
 lis=[]
-cnt =[0]*8000
+cnt =[0]*8001
 sum_s = 0
-max_s = -5000
-min_s = 5000
+
 for i in range(n):
-    s= int(stdin.readline())
+    s = int(stdin.readline())
     lis.append(s)
     cnt[s]+=1
     sum_s += s
-    if max_s < s:
-        max_s = s
-    if min_s > i:
-        min_s = s
-    
-s.sort()
 
 
+lis.sort()
 
 print(int(round(sum_s/n,0)))
-print(s[n//2])
-print(find(s))
-print(max_s-min_s)
+
+print(lis[len(lis)//2])
+
+max_cnt = max(cnt)
+number=[]
+if cnt.count(max_cnt) == 1:
+    ans = cnt.index(max_cnt)
+    if ans>4000:
+        ans=ans-8001
+    print(ans)
+else:
+    for i in range(len(cnt)):
+        if max_cnt == cnt[i] :
+            if i>4000:
+                number.append(i-8001)
+            else:
+                number.append(i)
+    #print(number)
+    number=list(set(number))
+    number.sort()
+    print(number[1])
+
+print(max(lis)-min(lis))
 
 
