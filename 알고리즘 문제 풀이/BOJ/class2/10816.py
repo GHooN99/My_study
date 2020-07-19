e6148597127f5bc7c4b82ac8b87772402d4e71d9
GHooN99 <whiteblue0316@gmail.com>
2020-07-19 23:56:14 +0900

    Fix 10816.py

--- 알고리즘 문제 풀이/BOJ/class2/10816.py
+++ 알고리즘 문제 풀이/BOJ/class2/10816.py
@@ -1,21 +1,18 @@
 from sys import stdin
-from collections import deque,Counter
-import bisect
 
-def solution(arr,num,ptrs):
+
+
+def solution(arr,num):
     start = 0
     end =len(arr)-1
     while True:
-        if ptrs==0:
-            ptr = (start+end)//2
-        else:
-            ptr=ptrs
-        
+        ptr = (start+end)//2
+
         if end-start<0:
             return False,0
         if arr[ptr]==num:
-            del arr[ptr]
-            return True,ptrs
+            cnt=arr[start:end+1]
+            return True,cnt.count(num)
         elif arr[ptr]>num:
             end = end-1
         else:
@@ -24,20 +21,18 @@ def solution(arr,num,ptrs):
 
 # input
 n = int(stdin.readline())
-arr = deque(sorted(map(int,stdin.readline().split())))
+arr = sorted(map(int,stdin.readline().split()))
 m = int(stdin.readline())
-answer = deque([0]*m)
-tar = deque(map(int,stdin.readline().split()))
+answer = [0]*m
+tar = list(map(int,stdin.readline().split()))
 
 
 
 for i,num in enumerate(tar):
-    idx=0
-    while True:
-        check,idx = solution(arr,num,idx)
-        if not check:
-            break
-        answer[i]+=1
+    check,cnt = solution(arr,num)
+    if check:
+        answer[i]+=cnt
+
 # output
 for i in answer:
     print(i,end=' ')
